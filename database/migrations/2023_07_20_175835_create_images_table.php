d4f64fa21eb3a2f1166ec848fd1e2da45baf7d7f
ДК <kolimbet-dv@yandex.ru>
2023-08-06 13:45:08 +0300

    Added migrations

---
+++ database/migrations/2023_07_20_175835_create_images_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('images', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
+            $table->foreignId('post_id')->nullable()->constrained('posts')->cascadeOnDelete();
+            $table->string('path');
+            $table->string('name');
+            $table->string('mime_type')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('images');
+    }
+};
---
+++ database/migrations/2023_07_20_181255_update_users_table.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::table('users', function (Blueprint $table) {
+      $table->boolean('is_admin')->default(false)->index();
+
+      $table->boolean('is_banned')->default(false)->index();
+      $table->foreignId('banned_by')->nullable()->constrained('users', 'id')->cascadeOnDelete();
+      $table->dateTime('ban_time')->nullable();
+      $table->text('ban_comment')->nullable();
+
+      $table->foreignId('avatar_id')->nullable()->constrained('images', 'id')->cascadeOnDelete();
+      $table->softDeletes();
+    });
+  }
+
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::table('users', function (Blueprint $table) {
+      $table->dropSoftDeletes();
+      $table->dropConstrainedForeignId('avatar_id');
+
+      $table->dropColumn('ban_comment');
+      $table->dropColumn('ban_time');
+      $table->dropConstrainedForeignId('banned_by');
+      $table->dropIndex(['is_banned']);
+      $table->dropColumn('is_banned');
+
+      $table->dropIndex(['is_admin']);
+      $table->dropColumn('is_admin');
+    });
+  }
+};
